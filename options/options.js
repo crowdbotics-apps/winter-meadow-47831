7a06c83f0bfa3824d94624ba8d63978814ba3661
Crowdbotics <team@crowdbotics.com>
2024-05-07 09:32:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-meadow-47831",
+  url: "https://winter-meadow-47831.botics.co",
+  api: "https://winter-meadow-47831.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
